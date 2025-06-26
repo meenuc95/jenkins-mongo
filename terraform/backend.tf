3b1120b19b01fffa1b0a6d0447f096ca499b30f8
meenuc95 <meenuc95@gmail.com>
2025-06-26 11:30:46 +0530

    initial

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "my-unique-bucket-name500"
     key            = "mongodb/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
