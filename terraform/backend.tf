8320e824bcd986b7c473554731f81fc8c5564e61
meenuc95 <meenuc95@gmail.com>
2025-06-28 12:24:23 +0530

    replica-fix

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "my-unique-bucket-name500"
-#     key            = "mongodb/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock"
-#     encrypt        = true
-#   }
-# }
